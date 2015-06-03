0bed67d9cbedcbaf71380c9c3160d9449eb5b590
Ryo Fukumuro <rkworks@o0o.co>
2015-06-04 03:39:00 +0900

    Fix version of example project

--- example/project.clj
+++ example/project.clj
@@ -1,6 +1,6 @@
 (defproject lein-baum-example "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :plugins [[rkworks/lein-baum "0.1.0-SNAPSHOT"]]
+  :plugins [[rkworks/lein-baum "0.1.0"]]
 
   :baum-path "example.edn"
   :port ^:baum/get-in [:webserver :port]
